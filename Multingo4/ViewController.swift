37f9caa2e335c2396682a12814b24e99214246b4
uries242 <teykoa@gmail.com>
2024-01-09 14:38:46 -0500

    Initial Commit

---
+++ Multingo4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multingo4
+//
+//  Created by Teyko on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
